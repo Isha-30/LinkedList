759890911e947afc4f46dd8bcfa969dd38e0a1ce
_isha_30 <choudhary.isha30@gmail.com>
2021-11-05 22:10:13 +0530

    Reverse ll using recursion

---
+++ ReverseLL/src/com/geekyscript/LinkedList.java
@@ -0,0 +1,43 @@
+package com.geekyscript;
+
+public class LinkedList {
+    Node head;
+    Node tail;
+    int size = 0;
+
+    void addLast(int data){
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.next = null;
+        if(head==null){
+            head = tail = newNode;
+        }
+        else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    void display(){
+        if(head==null)return;
+
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    private void ReverseHelper(Node node){
+        if(node==null)return;
+        ReverseHelper(node.next);
+        System.out.print(node.data +"->");
+    }
+
+    public void reverse(){
+        ReverseHelper(head);
+        System.out.println("null");
+    }
+}
